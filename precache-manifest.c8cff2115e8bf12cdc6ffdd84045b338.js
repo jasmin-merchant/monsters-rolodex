fedf7a7fbb3015881d27a9113bca19d0e829b538
System Administrator <root@Jasmins-MacBook-Air.local>
2019-08-18 18:43:10 +0530

    Updates

---
+++ precache-manifest.c8cff2115e8bf12cdc6ffdd84045b338.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dab3ea9935e93fda0f2cb63ab666b3d6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "663de9fdbb6bceb17114",
+    "url": "/monsters-rolodex/static/css/main.41a14b15.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "663de9fdbb6bceb17114",
+    "url": "/monsters-rolodex/static/js/main.19051b9b.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
